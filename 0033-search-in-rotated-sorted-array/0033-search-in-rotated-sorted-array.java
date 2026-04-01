e2a6fcee1bdfb2263a687eceb15b648b18696c50
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-04-01 13:35:05 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int st=0,end=nums.length-1;
+
+        while(st<=end){
+            int mid=st+(end-st)/2;
+
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]>nums[end]){
+                st=mid+1;
+            }else if(nums[mid]<nums[end]){
+                end=mid;
+            }else{
+                return -1;
+
+            }
+            
+
+        }
+        return st;
+    }
+}
