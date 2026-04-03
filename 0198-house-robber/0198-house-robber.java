30451f3cfe75df5f02074e01b82eb8d8e8ba7894
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-04-03 11:19:39 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.91%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -2,6 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         int n=nums.length;
         int dp[]=new int[n];
+        if(n==1)return nums[0];
 
         dp[0]=nums[0];
         dp[1]=Math.max(nums[1],nums[0]);
