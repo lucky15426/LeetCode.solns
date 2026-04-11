2e7348c1eb8c2fa638f829c8a8dd4262b0b504d2
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-04-11 11:00:37 +0530

    Time: 3 ms (80.88%), Space: 43.4 MB (82.6%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int n=s.length();
+        boolean dp[]=new boolean[n+1];
+
+        dp[0]=true;
+
+        for(int i=1;i<n+1;i++){
+            for(String w:wordDict){
+                int st=i-w.length();
+                if(st>=0 && dp[st] && s.substring(st,i).equals(w)){
+                    dp[i]=true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
+
+
+        
+    }
+}
