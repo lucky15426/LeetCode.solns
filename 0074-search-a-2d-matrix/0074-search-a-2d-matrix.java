2ad3fc251d99a049150dbb65eae73408f7cce27d
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-06-05 11:41:05 +0530

    Time: 0 ms (100%), Space: 43.6 MB (88.83%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,16 +1,17 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-        int row=matrix.length;
-        int col=matrix[0].length;
+        int n=matrix.length;
+        int m=matrix[0].length;
 
-       for(int i=0;i<row;i++){
-        for(int j=0;j<col;j++){
-            if(matrix[i][j]==target){
-                return true;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
             }
         }
-       }
-       return false;
+        return false;
+
         
     }
 }
