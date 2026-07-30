ed9ab890885774a15f3ee6da91e13660e65b3a75
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-07-30 11:53:32 +0530

    Time: 0 ms (100%), Space: 43.4 MB (28.23%) - LeetHub

---
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minimumPushes(String word) {
+        int n=word.length();
+        int max=Integer.MIN_VALUE;
+        if(n<=8)return n;
+
+        if(n>8)max=Math.max(max,(n-8)*2+8) ;
+        if(n>16)max=(n-16)+Math.max(max,(n-8)*2+8);
+        if(n>24)max=(n-24)+Math.max(max,(n-8)*2+8);
+        if(n>30)max=(n-30)+Math.max(max,(n-8)*2+8);
+
+        return max;
+        
+        
+    }
+}
