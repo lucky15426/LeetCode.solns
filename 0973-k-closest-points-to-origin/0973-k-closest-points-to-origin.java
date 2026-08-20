92597d44e06cc51f593c7ffdae8c7f2eda5cec23
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-20 09:38:10 +0530

    Time: 32 ms (37.16%), Space: 53.3 MB (89.8%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.java
@@ -1,25 +1,19 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((a, b) -> {
-            int distA = a[0] * a[0] + a[1] * a[1];
-            int distB = b[0] * b[0] + b[1] * b[1];
-            return distB - distA; // max heap
-        });
-
-        for (int i = 0; i < points.length; i++) {
+        PriorityQueue<int[]>pq=new PriorityQueue<>((a,b)->Integer.compare
+        (b[0]*b[0]+b[1]*b[1],a[0]*a[0]+a[1]*a[1]));
+        for(int i=0;i<points.length;i++){
             pq.add(points[i]);
 
-            if (pq.size() > k) {
-                pq.remove(); // removes farthest point
+            if(pq.size()>k){
+                pq.remove();
             }
         }
-
-        int[][] ans = new int[k][2];
-
-        for (int i = 0; i < k; i++) {
-            ans[i] = pq.remove();
+        int res[][]=new int[k][2];
+        for(int i=0;i<k;i++){
+            res[i]=pq.remove();
         }
+        return res;
 
-        return ans;
     }
 }
