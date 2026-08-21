d3fac22423bc57709b1f510351f17c5845496fdd
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-21 10:20:02 +0530

    Time: 2 ms (27.88%), Space: 42.8 MB (71.75%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.java
@@ -8,14 +8,16 @@ class MyStack {
     }
     
     public void push(int x) {
-        q2.offer(x);
-
+        q2.add(x);
         while(!q1.isEmpty()){
             q2.add(q1.remove());
         }
-        Queue<Integer>temp=q1;
+        Queue<Integer> temp=q1;
         q1=q2;
         q2=temp;
+
+
+        
         
     }
     
