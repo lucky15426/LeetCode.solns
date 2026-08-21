a141499292f38c148551d4b8b8ef56c783b4a28c
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-21 10:26:16 +0530

    Time: 2 ms (18.06%), Space: 43.1 MB (15.43%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -9,30 +9,29 @@ class MyQueue {
     }
     
     public void push(int x) {
+        while(!st2.isEmpty()){
+            st1.push(st2.pop());
+        }
+        st2.push(x);
+
         while(!st1.isEmpty()){
             st2.push(st1.pop());
         }
-
-        st1.push(x);
-
-       while(!st2.isEmpty()){
-        st1.push(st2.pop());
-       }
         
     }
     
     public int pop() {
-        return st1.pop();
+        return st2.pop();
         
     }
     
     public int peek() {
-        return st1.peek();
+        return st2.peek();
         
     }
     
     public boolean empty() {
-        return st1.isEmpty();
+        return st2.isEmpty();
         
     }
 }
