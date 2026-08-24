89c1d4da39c85c65d6050ad8a7b678c9c33839ce
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-24 19:28:23 +0530

    Time: 4 ms (99.05%), Space: 47.8 MB (29.24%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,29 +1,29 @@
 class Solution {
     public boolean validPalindrome(String s) {
-        int l=0,r=s.length()-1;
+        int n=s.length();
+        int l=0,r=n-1;
 
         while(l<r){
             if(s.charAt(l)!=s.charAt(r)){
-                return hehe(s,l+1,r)||hehe(s,l,r-1);
+                return isPalindrome(s,l+1,r)||isPalindrome(s,l,r-1);
 
             }
-            
             l++;
             r--;
+            
         }
         return true;
         
     }
-    private boolean hehe(String s,int l,int r){
+    private boolean isPalindrome(String s,int l,int r){
+        int n=s.length();
         while(l<r){
-        if(s.charAt(l)!=s.charAt(r))return false;
-
-        l++;
-        r--;
-
+            if(s.charAt(l)!=s.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
         }
- 
-
         return true;
     }
 }
