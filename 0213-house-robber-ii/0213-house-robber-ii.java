c65b980436fabe3acab80c728612d5ab0a269980
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-27 21:19:38 +0530

    Time: 0 ms (100%), Space: 42.7 MB (78.07%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,17 +1,17 @@
 class Solution {
     public int rob(int[] nums) {
-        int n=nums.length;
-        if(n==1)return nums[0];
-
-        return Math.max(HouseRobber1(nums,0,n-2),HouseRobber1(nums,1,n-1));
+        int n =nums.length;
         
+        if(nums.length==1)return nums[0];
+
+        return Math.max(Houserob1(nums,0,n-2),Houserob1(nums,1,n-1));
         
     }
-    private int HouseRobber1(int[]nums,int st,int end){
+    private int Houserob1(int[]nums,int st,int end){
         int n=end-st+1;
-        int dp[]=new int[n];
-
         if(n==1)return nums[st];
+
+        int dp[]=new int[n];
         dp[0]=nums[st];
         dp[1]=Math.max(nums[st],nums[st+1]);
 
