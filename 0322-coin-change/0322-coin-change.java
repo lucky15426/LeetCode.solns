5ac1916e6e1025f0f7096774b23bba22c1e963a3
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-28 20:50:41 +0530

    Time: 15 ms (83.38%), Space: 46.2 MB (73.37%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,12 +1,11 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
         int dp[]=new int[amount+1];
-
         Arrays.fill(dp,amount+1);
-
         dp[0]=0;
+        
 
-        for(int i=0;i<amount+1;i++){
+        for(int i=1;i<amount+1;i++){
             for(int coin:coins){
                 if(i-coin>=0){
                     dp[i]=Math.min(dp[i],dp[i-coin]+1);
@@ -16,6 +15,5 @@ class Solution {
             }
         }
         return dp[amount]==amount+1?-1:dp[amount];
-        
     }
 }
