d2e000454c28d7f9a5d8c630d74faa0db59a7877
Lucky Kumar Gupta <luckygupta1523@gmail.com>
2026-08-28 20:09:00 +0530

    Time: 6 ms (62.26%), Space: 42.5 MB (95.59%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,31 +1,25 @@
 class Solution {
     public int countSubstrings(String s) {
-        int n=s.length();
         int count=0;
-        for(int i=0;i<s.length();i++){
+        int n=s.length();
+
+        for(int i=0;i<n;i++){
             int l=i,r=i;
-
             while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
-                
-                    count++;
-                    l--;
-                    r++;
-                
-                
+                count++;
+                l--;
+                r++;
             }
-
-            l=i;
-            r=i+1;
-
-            while(l>=0 && r<n &&  s.charAt(l)==s.charAt(r)){
-               
-                    count++;
-                    l--;
-                    r++;
-                
-                
+             l=i;
+             r=i+1;
+            while(l>=0 && r<n && s.charAt(l)==s.charAt(r)){
+                count++;
+                l--;
+                r++;
             }
         }
+
         return count;
+
     }
 }
